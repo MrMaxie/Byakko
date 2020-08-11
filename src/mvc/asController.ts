0791e18ea1a54395854890ff4dda283e01b4e820
Maxie <mienkomaciej@gmail.com>
2020-08-11 16:45:29 +0200

    asController

--- src/mvc/asController.ts
+++ src/mvc/asController.ts
@@ -1,3 +1,20 @@
+type ControllerData = {
+    path?: string;
+    name?: string;
+    data?: {
+        [key: string]: any;
+    };
+};
+
+const Controllers = new Map<{ new(); }, ControllerData>();
+
+export const asController = (data: ControllerData = {}) => {
+    return (target: { new(); }) => {
+        Controllers.set(target, data);
+    };
+};
+
+asController.controllers = Controllers;
 
 @asController({
     path: '/',
